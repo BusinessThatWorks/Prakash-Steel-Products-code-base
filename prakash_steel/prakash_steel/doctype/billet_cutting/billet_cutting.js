7e358c062ade5c7f9c3f1babbc39820c8cafafff
ritika-ba <ritika@businessthat.works>
2025-08-29 16:35:05 +0530

    ritika

--- prakash_steel/prakash_steel/doctype/billet_cutting/billet_cutting.js
+++ prakash_steel/prakash_steel/doctype/billet_cutting/billet_cutting.js
@@ -6,27 +6,3 @@
 
 // 	},
 // });
-
-frappe.ui.form.on('Billet Cutting', {
-    refresh(frm) {
-        // Run check on refresh 
-        toggle_weight_field(frm);
-    },
-    miss_billet_pcs(frm) {
-        //  check whenever pcs changes
-        toggle_weight_field(frm);
-    }
-});
-
-function toggle_weight_field(frm) {
-    let pcs = frm.doc.miss_billet_pcs;
-
-    if (pcs && pcs > 0) {
-        frm.set_df_property("miss_billet_weight", "reqd", 1);
-        frm.set_df_property("miss_billet_weight", "hidden", 0);
-    } else {
-        frm.set_df_property("miss_billet_weight", "reqd", 0);
-        frm.set_df_property("miss_billet_weight", "hidden", 1);
-        frm.set_value("miss_billet_weight", null);
-    }
-}
---
+++ prakash_steel/prakash_steel/doctype/billet_cutting_item/billet_cutting_item.js
@@ -0,0 +1,32 @@
+// Copyright (c) 2025, beetashoke chakraborty and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Billet Cutting Item", {
+// 	refresh(frm) {
+
+// 	},
+// });
+
+frappe.ui.form.on('Billet Cutting Item', {
+    refresh(frm) {
+        // Run check on refresh 
+        toggle_weight_field(frm);
+    },
+    miss_billet_pcs(frm) {
+        //  check whenever pcs changes
+        toggle_weight_field(frm);
+    }
+});
+
+function toggle_weight_field(frm) {
+    let pcs = frm.doc.miss_billet_pcs;
+
+    if (pcs && pcs > 0) {
+        frm.set_df_property("miss_billet_weight", "reqd", 1);
+        frm.set_df_property("miss_billet_weight", "hidden", 0);
+    } else {
+        frm.set_df_property("miss_billet_weight", "reqd", 0);
+        frm.set_df_property("miss_billet_weight", "hidden", 1);
+        frm.set_value("miss_billet_weight", null);
+    }
+}
