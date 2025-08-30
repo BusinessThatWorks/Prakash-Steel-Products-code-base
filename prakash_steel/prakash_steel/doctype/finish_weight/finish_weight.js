8bd2cd5ee9f33d430a2db9109bdfa5590509c83d
clapgrowdev1 <clapgrowdev1@clapgrow.com>
2025-08-30 15:00:17 +0530

    finish weight js

--- prakash_steel/prakash_steel/doctype/finish_weight/finish_weight.js
+++ prakash_steel/prakash_steel/doctype/finish_weight/finish_weight.js
@@ -1,8 +1,17 @@
-// Copyright (c) 2025, beetashoke chakraborty and contributors
-// For license information, please see license.txt
+frappe.ui.form.on("Finish Weight Item", {
+    finish_weight: function(frm, cdt, cdn) {
+        calculate_fg_weight(frm, cdt, cdn);
+    },
+    finish_pcs: function(frm, cdt, cdn) {
+        calculate_fg_weight(frm, cdt, cdn);
+    }
+});
 
-// frappe.ui.form.on("Finish Weight", {
-// 	refresh(frm) {
-
-// 	},
-// });
+function calculate_fg_weight(frm, cdt, cdn) {
+    let row = frappe.get_doc(cdt, cdn);
+    if (row.finish_weight && row.finish_pcs) {
+        frappe.model.set_value(cdt, cdn, "fg_per_pcs_weight", row.finish_weight / row.finish_pcs);
+    } else {
+        frappe.model.set_value(cdt, cdn, "fg_per_pcs_weight", 0);
+    }
+}
