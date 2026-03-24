6e1b6927bab7e9d005f81ca80eff823ab3d69947
beetashoke <beetashoke.chakraborty@clapgrow.com>
2026-03-24 16:36:53 +0530

    bright bar and rolled bar report

--- prakash_steel/prakash_steel/report/bright_bar_sales_report/bright_bar_sales_report.js
+++ prakash_steel/prakash_steel/report/bright_bar_sales_report/bright_bar_sales_report.js
@@ -3,6 +3,39 @@
 
 frappe.query_reports["Bright Bar Sales report"] = {
 	"filters": [
-
+		{
+			"fieldname": "from_date",
+			"label": __("From Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.add_months(frappe.datetime.get_today(), -1),
+			"reqd": 0
+		},
+		{
+			"fieldname": "to_date",
+			"label": __("To Date"),
+			"fieldtype": "Date",
+			"default": frappe.datetime.get_today(),
+			"reqd": 0
+		},
+		{
+			"fieldname": "customer",
+			"label": __("Customer"),
+			"fieldtype": "Link",
+			"options": "Customer",
+			"reqd": 0
+		},
+		{
+			"fieldname": "item_code",
+			"label": __("Item Name"),
+			"fieldtype": "Link",
+			"options": "Item",
+			"reqd": 0
+		},
+		{
+			"fieldname": "category_name",
+			"label": __("Category Name"),
+			"fieldtype": "Data",
+			"reqd": 0
+		}
 	]
 };
--- prakash_steel/prakash_steel/report/rolled_bar_sales_report/rolled_bar_sales_report.py
+++ prakash_steel/prakash_steel/report/rolled_bar_sales_report/rolled_bar_sales_report.py
@@ -4,6 +4,120 @@
 # import frappe
 
 
+# def execute(filters=None):
+# 	columns, data = [], []
+# 	return columns, data
+
+
+# Copyright (c) 2026, Beetashoke Chakraborty and contributors
+# For license information, please see license.txt
+
+import frappe
+
+
 def execute(filters=None):
-	columns, data = [], []
+	filters = filters or {}
+
+	columns = get_columns()
+	data = get_data(filters)
+
 	return columns, data
+
+
+def get_columns():
+	return [
+		{"label": "Month", "fieldname": "month", "fieldtype": "Data", "width": 100},
+		{"label": "Date", "fieldname": "date", "fieldtype": "Date", "width": 100},
+		{
+			"label": "ID",
+			"fieldname": "id",
+			"fieldtype": "Link",
+			"options": "Sales Invoice",
+			"width": 160,
+		},
+		{
+			"label": "Sales Order",
+			"fieldname": "sales_order",
+			"fieldtype": "Link",
+			"options": "Sales Order",
+			"width": 160,
+		},
+		{
+			"label": "Customer",
+			"fieldname": "customer",
+			"fieldtype": "Link",
+			"options": "Customer",
+			"width": 180,
+		},
+		{"label": "Place of Supply", "fieldname": "place_of_supply", "fieldtype": "Data", "width": 130},
+		{"label": "Category Name", "fieldname": "category_name", "fieldtype": "Data", "width": 150},
+		{
+			"label": "Item Name",
+			"fieldname": "item_name",
+			"fieldtype": "Link",
+			"options": "Item",
+			"width": 200,
+		},
+		{"label": "Quantity", "fieldname": "qty", "fieldtype": "Float", "width": 100},
+		{"label": "Rate", "fieldname": "rate", "fieldtype": "Currency", "width": 120},
+		{
+			"label": "Warehouse",
+			"fieldname": "warehouse",
+			"fieldtype": "Link",
+			"options": "Warehouse",
+			"width": 180,
+		},
+	]
+
+
+def get_data(filters):
+	if not filters.get("from_date"):
+		filters["from_date"] = frappe.utils.add_months(frappe.utils.today(), -1)
+	if not filters.get("to_date"):
+		filters["to_date"] = frappe.utils.today()
+
+	conditions = """
+		si.docstatus = 1
+		AND si.posting_date BETWEEN %(from_date)s AND %(to_date)s
+		AND (i.custom_item_type = 'RB' OR i.custom_item_type = 'BO')
+	"""
+
+	if filters.get("customer"):
+		conditions += " AND si.customer = %(customer)s"
+
+	if filters.get("warehouse"):
+		conditions += " AND sii.warehouse = %(warehouse)s"
+
+	if filters.get("item_code"):
+		conditions += " AND sii.item_code = %(item_code)s"
+
+	if filters.get("category_name"):
+		conditions += " AND i.custom_category_name = %(category_name)s"
+
+	query = f"""
+		SELECT
+			DATE_FORMAT(si.posting_date, '%%b %%Y') AS month,
+			si.posting_date AS date,
+			si.name AS id,
+			sii.sales_order,
+			si.customer,
+			si.place_of_supply,
+			i.custom_category_name AS category_name,
+			sii.item_name,
+			sii.qty,
+			sii.rate,
+			sii.warehouse
+
+		FROM
+			`tabSales Invoice` si
+		LEFT JOIN
+			`tabSales Invoice Item` sii ON sii.parent = si.name
+		LEFT JOIN
+			`tabItem` i ON i.name = sii.item_code
+
+		WHERE {conditions}
+
+		ORDER BY si.posting_date DESC, si.name
+	"""
+
+	return frappe.db.sql(query, filters, as_dict=True)
