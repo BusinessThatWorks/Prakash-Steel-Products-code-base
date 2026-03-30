85a61a2c2bf20a85bbe10ef0e1d0978e019f5799
beetashoke <beetashoke.chakraborty@clapgrow.com>
2026-03-30 17:25:54 +0530

    from date to date filter part

--- prakash_steel/prakash_steel/report/po_recomendation_for_psp/po_recomendation_for_psp.js
+++ prakash_steel/prakash_steel/report/po_recomendation_for_psp/po_recomendation_for_psp.js
@@ -40,6 +40,18 @@ frappe.query_reports["PO Recomendation for PSP"] = {
 			options: "Item",
 			width: "80",
 		},
+		{
+			fieldname: "from_date",
+			label: __("From Date"),
+			fieldtype: "Date",
+			width: "80",
+		},
+		{
+			fieldname: "to_date",
+			label: __("To Date"),
+			fieldtype: "Date",
+			width: "80",
+		},
 	],
 
 	onload: function (report) {
