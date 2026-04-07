584f455afa659db65c9dc02c068571176268a104
clapgrowdev1 <clapgrowdev1@clapgrow.com>
2026-04-07 14:31:50 +0530

    add filter in Item Wise Stock Balance Report

--- prakash_steel/prakash_steel/report/item_wise_stock_balance/item_wise_stock_balance.js
+++ prakash_steel/prakash_steel/report/item_wise_stock_balance/item_wise_stock_balance.js
@@ -4,6 +4,23 @@
 frappe.query_reports["Item Wise Stock Balance"] = {
 	"filters": [
 		{
+			"fieldname": "item_code",
+			"label": __("Item Code"),
+			"fieldtype": "Link",
+			"options": "Item"
+		},
+		{
+			"fieldname": "item_group",
+			"label": __("Item Group"),
+			"fieldtype": "Link",
+			"options": "Item Group"
+		},
+		{
+			"fieldname": "category_name",
+			"label": __("Category Name"),
+			"fieldtype": "Data"
+		},
+		{
 			"fieldname": "include_zero_stock",
 			"label": __("Include Zero Stock Items"),
 			"fieldtype": "Check",
