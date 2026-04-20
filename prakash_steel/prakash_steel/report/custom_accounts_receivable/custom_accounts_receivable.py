3a11a77e10018fa76e14cad62776e67d020ebdc1
clapgrowdev1 <clapgrowdev1@clapgrow.com>
2026-04-20 09:58:11 +0530

    Custom Accounts Receivable Report and  Print Format.

---
+++ prakash_steel/prakash_steel/report/custom_accounts_receivable/custom_accounts_receivable.py
@@ -0,0 +1,4 @@
+# Copyright (c) 2026, Prakash Steel and contributors
+# Same logic as ERPNext Accounts Receivable (Script Report).
+
+from erpnext.accounts.report.accounts_receivable.accounts_receivable import execute
